b38e108b4f42e0feb70fc3dbcacad7b6cf6e6a51
Khaled Haimur <162723585+Khalidhaimur@users.noreply.github.com>
2024-11-05 22:05:59 +0300

    Add files via upload

---
+++ Project4.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+//This code defines a simple Java program that prints "Hi" followed by the current loop index, from 0 to 9.
+public class Project4 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.err.println("Hi " + i);
+        }
+    }
+}
