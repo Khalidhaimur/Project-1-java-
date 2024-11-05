5f60830f89dee6363cf71cfa2fb0ce653ee9ca17
Khaled Haimur <162723585+Khalidhaimur@users.noreply.github.com>
2024-11-05 21:55:31 +0300

    Add files via upload

---
+++ Project2.java
@@ -0,0 +1,23 @@
+public class Project2 {
+
+    public static void main(String[] args) {
+        int arr[] = new int[10];
+        
+        // إدخال القيم في المصفوفة
+        arr[0] = 20;
+        arr[1] = 32;
+        arr[2] = 54;
+        arr[3] = 33;
+        arr[4] = 66;
+        arr[5] = 45;
+        arr[6] = 21;
+        arr[7] = 62;
+        arr[8] = 53;
+        arr[9] = 18;
+        
+        // عرض القيم في المصفوفة
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("arr[" + i + "] = " + arr[i]);
+        }
+    }
+}
